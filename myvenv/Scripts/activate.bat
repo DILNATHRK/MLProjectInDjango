81b8f1e85d883d4268e91e6cf77693d880b932d0
DILNATHRK <rkdilnath@gmail.com>
2023-10-26 11:43:05 +0530

    myvenv recreated

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\RKD_FILES\MiniProjectInMachineLearning\FINAL\MlDjangoDOCKER\myvenv
+set VIRTUAL_ENV=D:\RKD_FILES\MiniProjectInMachineLearning\FINAL\MLDjango\MLProjectInDjangoDockeized\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
